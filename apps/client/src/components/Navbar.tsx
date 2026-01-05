4a24f09636b9fd4753e35b6b34f3d0cbd9e38731
Oleksandr Sukhinin <syhinin@gmail.com>
2026-01-05 22:02:13 +0100

    add Clerk authentication integration with sign-in and sign-up pages

--- apps/client/src/components/Navbar.tsx
+++ apps/client/src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 import SearchBar from "./SearchBar";
-import { Bell, Home, ShoppingCart } from "lucide-react";
+import { Bell, Home } from "lucide-react";
 import ShoppingCartIcon from "./ShoppingCartIcon";
 
 const Navbar = () => {
@@ -28,7 +30,12 @@ const Navbar = () => {
         </Link>
         <Bell className="w-4 h-4 text-gray-600" />
         <ShoppingCartIcon />
-        <Link href="/login">Sign in</Link>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
